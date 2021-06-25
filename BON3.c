327fc74c3f693a66d16d8d7914fcb8a24da22e81
bonface769 <85347045+bonface769@users.noreply.github.com>
2021-06-25 13:40:12 +0300

    Add files via upload

---
+++ BON3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+int a=10;
+printf("value of a");
+scanf("%d,&a");
+a+=10;
+
+	return 0;
+}
---
+++ BON4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+int m=10,n=10;
+if(m==n)
+{
+	printf("m and n are equal");
+}
+else
+{
+	printf("m and n are not equal");
+}
+	return 0;
+}
